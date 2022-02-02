e71ca38372b73bdf94b7d195969469c1f8553350
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-01 17:55:53 -0800

    remove extraneous required modules and file paths, clean up code, add comments for readability

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-// Import (require) all the goodies (node modules)
-const fs = require("fs");
+// Import all the node goodies (modules)
 const conTable = require('console.table');
-const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 //const mysql = require('mysql2/promise'); 
-const internal = require("stream");
 
-
-//import required files 
-const add = require("./lib/add");
+const questions = require("./lib/questions"); //import required files 
+//const add = require("./lib/add");
 //const view = require("./lib/view");
-const questions = require("./lib/questions");
-const { response } = require("express");
-//const { exit } = require("process");
 
 
-//const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
+// Connect to the database of departments, roles, and employees that makes up this company
 const company = mysql.createConnection(
     {
       host: 'localhost',
@@ -34,14 +20,12 @@ const company = mysql.createConnection(
     console.log(`Connected to the company_db database.`)
 );
   
-const init = () => {
-    //prompt user for what they would like to do 
+const init = () => { //prompt user for what they would like to do 
+
     inquirer.prompt(questions.startQuestion).then((response) => {
         
         //object destructing to pull out repsonse 
         const { startOption }  = response;
-
-        console.log (startOption);
      
         //switch statement to call fxn based on input 
         switch (startOption) {
@@ -77,74 +61,79 @@ const init = () => {
     }) 
 }
 
-const viewTableFunction = (table_name) => {
+//function to display a table from sql prepared statement, gets passed the table name as an argument
+const viewTableFunction = (table_name) => {  
     company.query(`SELECT * FROM ${table_name}`, function (err, results) {
         console.log ("\n")
-        console.table(results);
-        continueQ();
+        console.table(results);  //console.table makes a nice display of tables from the command line 
+        continueQ();  //once table is displayed, prompt user if they would like to quit or continue
     });
 }
 
 const addDepartment = () => {
+    //ask the array of inquirer questions for a new department, imported from questions.js
     inquirer.prompt(questions.department).then((response) => {
 
         const sql = `INSERT INTO department (department_name) VALUES (?)`;
-        const department_name = [response.department];
-            
+        const department_name = [response.department]; //assign response into new array
+        
+        //query the company database for the row insert that we need
         company.query(sql, department_name, (err, result) => {
             if (err) {
                 console.log.json({ error: err.message });
                 return;
             }
             console.log(`All Departments (with newly added ${department_name}`)
-            viewTableFunction("department");
+            viewTableFunction("department"); 
         });
     });
 };
 
 const addRole = () => {
+     //ask the array of inquirer questions for a new role, imported from questions.js
     inquirer.prompt(questions.role).then((response) => {
             
-        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-        const newRole = [
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;  //sql preapred statement 
+        const newRole = [   //assign response into new array
             response.role, 
             response.salary, 
             response.department_id,
         ]
 
+        //query the company database for the row insert that we need
         company.query(sql, newRole, (err, result) => {
             if (err) {
                 console.log({ error: err.message });
                 return;
             }
             console.log(`All Roles (plus newly added role)`)
-            viewTableFunction("role");
+            viewTableFunction("role"); //show updated table 
         });
     });
 };
 
 const addEmployee = () => {
+     //ask the array of inquirer questions for a new employee, imported from questions.js
     inquirer.prompt(questions.employee).then((response) => {
 
-        console.log(response);
+        //console.log(response);
 
         const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-        const newEmployee = [
+        const newEmployee = [ //assign response into new array
             response.first_name, 
             response.last_name, 
             response.role_id, 
             response.manager_id,
         ]
 
-        console.log(newEmployee);
-
+        //query the company database for the row insert that we need
         company.query(sql, newEmployee, (err, result) => {
             if (err) {
                 console.log({ error: err.message });
                 return;
             }
             console.log(`All Employees (plus newly added employee)`)
-            viewTableFunction("employee");
+            viewTableFunction("employee"); //show updated table 
         });
     });
 };
@@ -153,42 +142,34 @@ const updateEmployee = () => {
 
     inquirer.prompt(questions.updateRoleQuestions).then((response) => {
 
-        console.log(response);
-
+        //console.log(response);
         const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
         const update = [ response.role_id, response.id ]
 
-        console.log(update);
-
+        //query the company database for the row insert that we need with update sql preapred statement 
         company.query(sql, update, (err, result) => {
             if (err) {
                 console.log({ error: err.message });
                 return;
             }
             console.log(` Employee's role has been updated.`)
-            viewTableFunction("employee");
+            viewTableFunction("employee"); //show updated table 
         });
     });
-
-
-    
 }
 
-
-
-const continueQ = () => {
+const continueQ = () => {  //prompt the user if they would like to continue or quit
     inquirer.prompt(questions.continueYN).then((response) => {
-        if (response.continue === 'yes'){  
-            init(); 
+        if (response.continue === 'continue'){  
+            init();  //ask start option question again. 
         } else {
-            console.log("Thank you for using this program.");
-            process.exit();
+            console.log("Thank you for using this program! \n");
+            process.exit(); //exit application 
         }
     })   
 }
 
-
-init();
+init();  //call the first function, asking the user what work they would like to perform 
 
     
 
